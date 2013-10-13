320e4e92cc720aa04c7acbdc0789e5e3cf26aa36
dparalen <vetrisko@gmail.com>
2013-10-13 15:51:24 +0200

    introducing race test cases module

---
+++ tests/test_3_race.py
@@ -0,0 +1,27 @@
+import pulp_test, json
+from pulp_test import PulpTest
+from pulp.repo import Repo, Importer
+from pulp.task import Task, GroupTask
+from pulp import ResponseLike, login, format_response
+
+class RepoTest(pulp_test.PulpTest):
+    def setUp(self):
+        super(RepoTest, self).setUp()
+        self.repo = Repo(data={'id': type(self).__name__ + "_repo"})
+        self.feed = 'http://repos.fedorapeople.org/repos/pulp/pulp/demo_repos/zoo/'
+        # assert connection works
+        self.pulp.send(login.request())
+        self.assertPulpOK()
+
+    def tearDown(self):
+        self.repo.delete(self.pulp)
+
+
+class RaceRepoTest(RepoTest):
+    def test_01_race_create_delete(self):
+        with self.pulp.async():
+            self.repo.create(self.pulp)
+            self.repo.create(self.pulp)
+        self.assertIn(ResponseLike(status_code=409), self.pulp.last_response)
+        self.assertIn(ResponseLike(status_code=201), self.pulp.last_response)
+
