b121287049ca7a04c61f440cd66e7bab7237f1d6
Ina Panova <ipanova@redhat.com>
2013-10-16 23:52:35 +0200

    Updated User item with the update function

--- pulp/user.py
+++ pulp/user.py
@@ -2,10 +2,11 @@ import item, json
 from pulp import (Request, )
 from . import (path_join, format_response)
 
+
 class User(item.Item):
-    relevant_data_keys = ['login', 'name', 'password']
+    relevant_data_keys = ['login', 'name']
     required_data_keys = ['login']
-    path='/users/'
+    path = '/users/'
 
     @property
     def id(self):
@@ -15,3 +16,18 @@ class User(item.Item):
     def id(self, other):
         self.data['login'] = other
 
+    def update(self, pulp):
+        item = self.get(pulp, self.id)
+        # update call requires a delta-data dict; computing one based on data differences
+        # note that id shouldn't appear in the delta since the get is using it
+        delta = {
+            'delta': self.delta(item)
+        }
+
+        if 'password' in self.data:
+            # password has to be updated all the time
+            delta['delta']['password'] = self.data['password']
+
+        return pulp.send(
+            self.request('PUT', data=delta)
+        )
