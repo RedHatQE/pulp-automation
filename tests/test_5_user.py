8f35b6030debce87ec1ff4bd2406a5965cf0c5d8
Ina Panova <ipanova@redhat.com>
2013-10-16 23:53:35 +0200

    expanded test_5_user.py , also added user_pulp session

--- tests/test_5_user.py
+++ tests/test_5_user.py
@@ -1,9 +1,11 @@
 import pulp_test, json
 from pulp.user import User
-from pulp import login, Pulp
+from pulp import login, Pulp, format_response, format_preprequest
+
 
 def setUpModule():
     pass
+
 
 class UserTest(pulp_test.PulpTest):
     @classmethod
@@ -11,7 +13,7 @@ class UserTest(pulp_test.PulpTest):
         super(UserTest, cls).setUpClass()
         cls.user = User(data={"login": cls.__name__ + "_user", "name": cls.__name__, "password": cls.__name__})
         # a new session has to be created for the user as auth credeantials of admin are used by default
-        cls.user_pulp = Pulp(cls.pulp.url, auth = (cls.user.data['login'], cls.user.data['password']))
+        cls.user_pulp = Pulp(cls.pulp.url, auth=(cls.user.data['login'], cls.user.data['password']))
 
 
 class SimpleUserTest(UserTest):
@@ -20,30 +22,37 @@ class SimpleUserTest(UserTest):
         self.user.create(self.pulp)
         self.assertPulpOK()
 
-    def test_02_update_user(self):
+    def test_02_get_user(self):
+        user = User.get(self.pulp, self.user.id)
+        self.assertEqual(self.user, user)
+
+    def test_03_list_users(self):
+        users = User.list(self.pulp)
+        self.assertIn(self.user, users)
+
+    def test_04_update_user(self):
         name = 'name %s' % self.__class__.__name__
         self.user |= {'name': name}
         self.user.update(self.pulp)
         self.assertPulp(code=200)
         self.assertEqual(User.get(self.pulp, self.user.id).data['name'], name)
 
-    def test_03_update_password_user(self):
+    def test_05_update_password_user(self):
         password = 'password'
-        self.user |= {'password': password}
+        self.user.data["password"] = password
         self.user.update(self.pulp)
         self.assertPulp(code=200)
         # checking if user cannot login with old password
         with self.assertRaises(AssertionError):
             with self.user_pulp.asserting(True):
-	        self.user_pulp.send(login.request())
+                self.user_pulp.send(login.request())
         #checking if the user can login with new password
         self.user_pulp.auth = (self.user.data['login'], password)
-        # self.assertPulpOK() cannot be used here
+        # self.assertPulpOK() cannot be used here, as we have to check response from user session, not admin session
         with self.user_pulp.asserting(True):
             self.user_pulp.send(login.request())
         # FIXME the password is not restored to the default one
 
-
-    def test_04_delete_user(self):
+    def test_06_delete_user(self):
         self.user.delete(self.pulp)
         self.assertPulpOK()
