c5e4e69ade02443529b1a9a48b40bd6fb109b408
Ina Panova <ipanova@redhat.com>
2013-10-22 13:37:09 +0200

    Introducing permission item

---
+++ pulp/permission.py
@@ -0,0 +1,25 @@
+import item, json
+from pulp import (Request, path_join)
+
+
+class Permission(item.Item):
+    relevant_data_keys = ['resource', 'users']
+    required_data_keys = ['resource']
+    path = '/permissions/'
+
+    @property
+    def id(self):
+        return self.data['resource']
+
+    @id.setter
+    def id(self, other):
+        self.data['resource'] = other
+
+    def create(self, pulp):
+        raise TypeError('Cannot create new permissions. Valid permissions are [CREATE, READ, UPDATE, DELETE, EXECUTE]')
+
+    def delete(self, pulp):
+        raise TypeError('Cannot create new permissions. Valid permissions are [CREATE, READ, UPDATE, DELETE, EXECUTE]')
+
+    def update(self, pulp):
+        raise TypeError('Cannot create new permissions. Valid permissions are [CREATE, READ, UPDATE, DELETE, EXECUTE]')
