250f9871f44a325900ab4e90640e39dac96727ff
dparalen <vetrisko@gmail.com>
2013-12-05 18:15:57 +0100

    introducing implicit certificate saving

--- pulp_auto/consumer.py
+++ pulp_auto/consumer.py
@@ -13,6 +13,16 @@ class Consumer(Item):
     path = '/consumers/'
     relevant_data_keys = ['id', 'display_name']
 
+    def create(self, pulp):
+        '''creating consumer gives vital data: private key in cert; one can't fetch it otherwise
+        The private key is preserved by an implicit self.data update. A reload destroys it, though.
+        '''
+        response = super(Consumer, self).create(pulp)
+        tmp_consumer = type(self).from_response(response)
+        self.data = tmp_consumer.data
+        return response
+        
+
     def bind_distributor(self, pulp, data):
         '''bind this consumer to a repo distributor'''
         return pulp.send(self.request('POST', path=Binding.path, data=data))
@@ -27,6 +37,13 @@ class Consumer(Item):
     def get_repo_bindings(self, pulp, repo_id):
         return Binding.from_response(pulp.send(self.request('GET', path=path_join(Binding.path, repo_id))))
 
+    @property
+    def certificate(self):
+        return self.data['certificate']
+
+    @certificate.setter
+    def certificate(self, cert):
+        self.data['certificate'] = cert
 
 SAMPLE_DISTRIBUTOR_BIND_DATA = {
     "repo_id": "test-repo",
