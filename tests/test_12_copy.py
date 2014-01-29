1e087cb07468e6175116ede662d9e054610813f3
Ina Panova <ipanova@redhat.com>
2014-01-29 21:14:31 +0100

    Updated testcase to comply with changes made to create_yum_repo()

--- tests/test_12_copy.py
+++ tests/test_12_copy.py
@@ -15,22 +15,22 @@ class SimpleRepoCopyTest(pulp_test.PulpTest):
         #Destination repo
         feed = None
         dest_repo_name = cls.__name__ + '_copy'
+        distributor_name_id = 'dist_1'
         dest_repo1 = Repo({'id': dest_repo_name})
         dest_repo1.delete(cls.pulp)
-        cls.dest_repo1 = create_yum_repo(cls.pulp, dest_repo_name, feed, relative_url=dest_repo_name)[0]
+        cls.dest_repo1 = create_yum_repo(cls.pulp, dest_repo_name, distributor_name_id, feed)[0]
 
         #2nd Destination Repo
         dest_repo_name = cls.__name__ + '_copy1'
         dest_repo2 = Repo({'id': dest_repo_name})
         dest_repo2.delete(cls.pulp)
-        cls.dest_repo2 = create_yum_repo(cls.pulp, dest_repo_name, feed, relative_url=dest_repo_name)[0]
+        cls.dest_repo2 = create_yum_repo(cls.pulp, dest_repo_name, 'dist_2', feed)[0]
 
         # Source repo
-        feed = 'http://repos.fedorapeople.org/repos/pulp/pulp/demo_repos/zoo/'
         source_repo_name =  cls.__name__ + '_repo'
         source_repo = Repo({'id': source_repo_name})
         source_repo.delete(cls.pulp)
-        cls.source_repo = create_yum_repo(cls.pulp, source_repo_name, feed, relative_url=source_repo_name)[0]
+        cls.source_repo = create_yum_repo(cls.pulp, source_repo_name, 'dist_3')[0]
         sync_task = Task.from_response(cls.source_repo.sync(cls.pulp))[0]
         sync_task.wait(cls.pulp)
 
