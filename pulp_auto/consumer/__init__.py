b18910b1323709a7528eca57fbda44a85be167d8
dparalen <vetrisko@gmail.com>
2014-01-29 16:22:57 +0100

    propagating namespace

--- pulp_auto/consumer/__init__.py
+++ pulp_auto/consumer/__init__.py
@@ -1,2 +1,4 @@
 from consumer_class import (Binding, Consumer, SAMPLE_DISTRIBUTOR_BIND_DATA)
-from cli_class import (Cli,)
+from cli_class import (Cli, ConsumerCommand)
+from unit import (RpmUnit,)
+from repo import (BaseRepo, RpmRepo, PuppetRepo, YumRepo)
