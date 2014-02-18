5fff72a1fd6e2b566032f347384029be0cc57ebf
Ina Panova <ipanova@redhat.com>
2014-02-18 14:31:03 +0100

    Expanded login test with wrong credentials

--- tests/test_1_log_in.py
+++ tests/test_1_log_in.py
@@ -1,9 +1,25 @@
 import pulp_test
-from pulp_auto import login
+from pulp_auto import login, Pulp
+
+
+def setUpModule():
+    pass
 
 
 class TestLogin(pulp_test.PulpTest):
+    @classmethod
+    def setUpClass(cls):
+        super(TestLogin, cls).setUpClass()
+        cls.wrong_pulp = Pulp(cls.pulp.url, auth=('admin', 'xxx'))
+
+
+class SimpleTestLogin(TestLogin):
 
     def test_login(self):
         self.pulp.send(login.request())
         self.assertPulpOK()
+
+    def test_wrong_login(self):
+        with self.assertRaises(AssertionError):
+            with self.wrong_pulp.asserting(True):
+                self.wrong_pulp.send(login.request())
