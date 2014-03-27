0851229af05576fc5b9fc40798e97be5f2bed909
Ina Panova <ipanova@redhat.com>
2014-03-27 16:04:47 +0100

    test_2_rpm_repo.py: expanded with retrieval tescases

--- tests/test_2_rpm_repo.py
+++ tests/test_2_rpm_repo.py
@@ -13,6 +13,7 @@ class RepoTest(pulp_test.PulpTest):
     def setUpClass(cls):
         super(RepoTest, cls).setUpClass()
         cls.repo = Repo(data={'id': cls.__name__ + "_repo"})
+        cls.repo2 = Repo(data={'id': cls.__name__ + "_repo2"})
         cls.feed = 'http://repos.fedorapeople.org/repos/pulp/pulp/demo_repos/zoo/'
 
 
@@ -21,12 +22,19 @@ class SimpleRepoTest(RepoTest):
     def test_01_create_repo(self):
         self.repo.create(self.pulp)
         self.assertPulpOK()
+        #no duplicate repo
+        self.repo.create(self.pulp)
+        self.assertPulp(code=409)
 
     def test_02_get_repo(self):
         repo = Repo.get(self.pulp, self.repo.id)
         self.assertEqual(repo.id, self.repo.id)
         self.repo.reload(self.pulp)
         self.assertEqual(self.repo, repo)
+        #get unexistant repo
+        with self.assertRaises(AssertionError):
+            Repo.get(self.pulp, 'some_id')
+        self.assertPulp(code=404)
 
     def test_03_list_repos(self):
         repos = Repo.list(self.pulp)
@@ -39,7 +47,27 @@ class SimpleRepoTest(RepoTest):
         self.assertPulp(code=200)
         self.assertEqual(Repo.get(self.pulp, self.repo.id).data['display_name'], display_name)
 
-    def test_05_associate_importer(self):
+    def test_05_list_importer(self):
+        importer = self.repo.list_importers(self.pulp)
+        self.assertPulp(code=200)
+        self.assertEqual(importer, [])
+
+    def test_05_list_importer_of_unexistant_repo(self):
+        with self.assertRaises(AssertionError):
+            self.repo2.list_importers(self.pulp)
+        self.assertPulp(code=404)
+
+    def test_06_list_distributors(self):
+        distributors = self.repo.list_distributors(self.pulp)
+        self.assertPulp(code=200)
+        self.assertEqual(distributors, [])
+
+    def test_06_list_distributors_of_unexistant_repo(self):
+        with self.assertRaises(AssertionError):
+            self.repo2.list_distributors(self.pulp)
+        self.assertPulp(code=404)
+
+    def test_07_associate_importer(self):
         response = self.repo.associate_importer(
             self.pulp,
             data={
@@ -64,7 +92,12 @@ class SimpleRepoTest(RepoTest):
             },
             importer)
 
-    def test_06_associate_distributor(self):
+    def test_08_get_single_importer(self):
+        importer=self.repo.get_importer(self.pulp, "yum_importer")
+        importers = self.repo.list_importers(self.pulp)
+        self.assertEqual(importer,importers[0])
+
+    def test_09_associate_distributor(self):
         response = self.repo.associate_distributor(
             self.pulp,
             data={
@@ -96,12 +129,17 @@ class SimpleRepoTest(RepoTest):
             }
         )
 
-    def test_07_sync_repo(self):
+    def test_10_get_single_distributor(self):
+        distributor = self.repo.get_distributor(self.pulp, "dist_1")
+        distributors = self.repo.list_distributors(self.pulp)
+        self.assertIn(distributor, distributors)
+
+    def test_11_sync_repo(self):
         response = self.repo.sync(self.pulp)
         self.assertPulp(code=202)
         Task.wait_for_report(self.pulp, response)
 
-    def test_08_publish_repo(self):
+    def test_12_publish_repo(self):
         response = self.repo.publish(
             self.pulp,
             'dist_1'
@@ -109,14 +147,14 @@ class SimpleRepoTest(RepoTest):
         self.assertPulp(code=202)
         Task.wait_for_report(self.pulp, response)
 
-    def test_09_delete_repo(self):
+    def test_13_delete_repo(self):
         response = self.repo.delete(self.pulp)
         Task.wait_for_report(self.pulp, response)
         #check you cannot delete it twice
         self.repo.delete(self.pulp)
         self.assertPulp(code=404)
 
-    def test_10_delete_orphans(self):
+    def test_14_delete_orphans(self):
         delete_response = Orphans.delete(self.pulp)
         self.assertPulpOK()
         Task.wait_for_report(self.pulp, delete_response)
