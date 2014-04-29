8aa259ef6a17ff4aecac3c300d00c8d86d7be14c
Ina Panova <ipanova@redhat.com>
2014-04-29 15:39:50 +0200

    Expanded oprhan testcases

--- tests/test_10_orphans.py
+++ tests/test_10_orphans.py
@@ -1,6 +1,8 @@
 import pulp_test, json, pprint, pulp_auto
 from pulp_auto.repo import create_yum_repo, Repo
 from pulp_auto.task import Task
+from pulp_auto.pulp import Request
+from pulp_auto import path_join
 from pulp_auto.units import Orphans, UnitFactory, RpmOrphan, PackageGroupOrphan, PackageCategoryOrphan, ErratumOrphan, DistributionOrphan, DrpmOrphan, SrpmOrphan, YumRepoMetadataFileOrphan,PuppetModuleOrphan, IsoOrphan
 from . import ROLES
 
@@ -24,9 +26,26 @@ class SimpleOrphanTest(pulp_test.PulpTest):
         # this is where orphans appear
         Task.wait_for_report(cls.pulp, cls.repo.delete(cls.pulp))
 
-    def test_00_get_orphan_info(self):
+    def test_00_view_all_orphaned_content(self):
         Orphans.info(self.pulp)
         self.assertPulpOK()
+
+    def test_01_view_orphaned_content_by_type(self):
+        rpm_orphans = RpmOrphan.list(self.pulp)
+        self.assertPulpOK()
+        all_orphans = Orphans.info(self.pulp)
+        self.assertEqual(len(rpm_orphans), all_orphans['rpm']['count'])
+
+    def test_01_view_orphaned_content_invalid_type(self):
+        # https://bugzilla.redhat.com/show_bug.cgi?id=1092450
+        self.pulp.send(Request('GET', path_join(Orphans.path, 'invalid_type')))
+        self.assertPulp(code=404)        
+
+    def test_01_view_individual_orphan(self):
+        # that does not exist
+        with self.assertRaises(AssertionError):
+            RpmOrphan.get(self.pulp, 'some_id')
+        self.assertPulp(code=404)
 
     def test_01_orphan_info_data_integrity(self):
         info = Orphans.info(self.pulp)
@@ -56,6 +75,7 @@ class SimpleOrphanTest(pulp_test.PulpTest):
             sorted(map(lambda x: x.data['name'], RpmOrphan.list(self.pulp)))
         )
 
+
     def test_03_delete_orphans(self):
         delete_response = Orphans.delete(self.pulp)
         self.assertPulpOK()
@@ -70,6 +90,30 @@ class SimpleOrphanTest(pulp_test.PulpTest):
     def test_04_create_orphans_again(self):
         # first a repo created, synced and then deleted
         self.setUpClass()
+
+    def test_05_delete_orphan_by_invalid_type(self):
+        # https://bugzilla.redhat.com/show_bug.cgi?id=1092460
+        self.pulp.send(Request('DELETE', path_join(Orphans.path, 'invalid_type')))
+        self.assertPulp(code=404)
+
+    def test_05_delete_individual_orphan(self):
+        rpm_orphans = RpmOrphan.list(self.pulp)
+        unit_id = rpm_orphans[0]
+        response = unit_id.delete(self.pulp)
+        self.assertPulpOK()
+        Task.wait_for_report(self.pulp, response)
+
+    def test_05_delete_individual_non_exitent_orphan(self):
+        # https://bugzilla.redhat.com/show_bug.cgi?id=1092460
+        self.pulp.send(Request('DELETE', path_join(RpmOrphan.path, 'some_unit_id')))
+        self.assertPulp(code=404)
+
+    def test_05_delete_orphan_by_type_and_id(self):
+        # https://bugzilla.redhat.com/show_bug.cgi?id=1092467
+        response = Orphans.delete_by_type_id(self.pulp, data=[{'content_type_id': 'rpm', 'unit_id': 'd0dc2044-1edc-4298-bf10-a472ea943fe1'}]
+                                            )
+        self.assertPulpOK()
+        Task.wait_for_report(self.pulp, response)
 
     def test_05_delete_orphan_rpm(self):
         RpmOrphan.delete_all(self.pulp)
