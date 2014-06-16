218aef1323b6e756aa9294c5bf7ab402447edf41
Ina Panova <ipanova@redhat.com>
2014-06-16 15:46:44 +0200

    Removing  test_03_race_delete_published due to resolution of this bz1065455

--- tests/test_3_race.py
+++ tests/test_3_race.py
@@ -28,6 +28,7 @@ class RaceRepoTest(PulpTest):
         self.assertIn(ResponseLike(status_code=201), self.pulp.last_response)
 
     def test_02_race_delete(self):
+        # see also: https://bugzilla.redhat.com/show_bug.cgi?id=1065455
         repo = Repo(data=self.repo_config)
         with self.pulp.async():
             repo.delete(self.pulp)
@@ -36,24 +37,6 @@ class RaceRepoTest(PulpTest):
         self.assertIn(ResponseLike(status_code=202), self.pulp.last_response)
         task_responses = filter(lambda response: response == ResponseLike(status_code=202), self.pulp.last_response)
         not_responses = filter(lambda response: response == ResponseLike(status_code=404), self.pulp.last_response)
-        self.assertEqual(len(task_responses), 1, "There should be just a single task response")
-        for response in task_responses:
-            Task.wait_for_report(self.pulp, response)
-
-    def test_03_race_delete_published(self):
-        # repos with associated importers/distributors are deleted asynchronously; a 409 may happen
-        # see also: https://bugzilla.redhat.com/show_bug.cgi?id=1065455
-        repo, _, distributor = create_yum_repo(self.pulp, **self.repo_config)
-        repo.sync(self.pulp)
-        repo.publish(self.pulp, distributor.id)
-        with self.pulp.async():
-            repo.delete(self.pulp)
-            repo.delete(self.pulp)
-        self.assertIn(ResponseLike(status_code=409), self.pulp.last_response)
-        self.assertIn(ResponseLike(status_code=202), self.pulp.last_response)
-
-        task_responses = filter(lambda response: response == ResponseLike(status_code=202), self.pulp.last_response)
-        not_responses = filter(lambda response: response == ResponseLike(status_code=409), self.pulp.last_response)
         self.assertEqual(len(task_responses), 1, "There should be just a single task response")
         for response in task_responses:
             Task.wait_for_report(self.pulp, response)
