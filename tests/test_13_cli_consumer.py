de7b5548810dca33263659020c1bab5fc0d05117
dparalen <vetrisko@gmail.com>
2014-06-20 14:09:31 +0200

    introducing multi-consumer support

--- tests/test_13_cli_consumer.py
+++ tests/test_13_cli_consumer.py
@@ -16,75 +16,96 @@ class CliConsumerTest(PulpTest):
     @classmethod
     def setUpClass(cls):
         super(CliConsumerTest, cls).setUpClass()
-        consumer_config = ROLES.consumers[0]
         # create all repos
+        # get repo configs across all consumers
+        repo_configs = sum([consumer.repos for consumer in ROLES.consumers], [])
+        # filter&uniq repo configs
+        repo_configs = {repo.id: repo for repo in repo_configs if repo.type == 'rpm'}.values()
         with cls.pulp.asserting(True):
-            cls.repos = [create_yum_repo(cls.pulp, **repo) for repo in consumer_config.repos if repo.type == 'rpm']
-            for repo, _, distributor in cls.repos:
-                Task.wait_for_report(cls.pulp, repo.sync(cls.pulp))
-                Task.wait_for_report(cls.pulp, repo.publish(cls.pulp, distributor.id))
-        cls.consumer_cli = Cli.ready_instance(**consumer_config)
-        cls.consumer = Consumer(consumer_config)
+            cls.repos = [ create_yum_repo(cls.pulp, **repo_config) for repo_config in repo_configs ]
+
+        # sync&publish all repos
+        with cls.pulp.asserting(True):
+            task_reports = [repo.sync(cls.pulp) for repo, _, _ in cls.repos]
+        Task.wait_for_reports(cls.pulp, task_reports)
+
+        with cls.pulp.asserting(True):
+            task_reports = [ repo.publish(cls.pulp, distributor.id) for repo, _, distributor in cls.repos]
+        Task.wait_for_reports(cls.pulp, task_reports)
+
+        # create all consumers
+        # gather all consumers
+        consumer_configs = {consumer.id: consumer for consumer in ROLES.consumers}
+        with cls.pulp.asserting(True):
+            cls.consumers = [Consumer(consumer_config) for consumer_config in consumer_configs.values()]
+
+        # set up consumer cli & link repos
+        for consumer in cls.consumers:
+            setattr(consumer, 'cli', Cli.ready_instance(**consumer_config))
+            setattr(
+                consumer,
+                'repos',
+                filter(
+                    lambda (repo, importer, distributor): any(repo.id == repo_config.id for repo_config in consumer_configs[consumer.id].repos),
+                    cls.repos
+                )
+            )
 
     def test_01_api_registered_consumer(self):
         # assert the cli registration worked in API
-        Consumer.get(self.pulp, self.consumer_cli.consumer_id)
-        self.assertPulpOK()
+        with self.pulp.asserting(True):
+            for consumer in self.consumers:
+                Consumer.get(self.pulp, consumer.cli.consumer_id)
 
     def test_02_bind_distributor(self):
         # assert binding distributor works
         # bind all repos
         with self.pulp.asserting(True):
-            for repo, _, distributor in self.repos:
-                Task.wait_for_report(self.pulp, self.consumer.bind_distributor(self.pulp, repo.id, distributor.id))
+           task_reports = [consumer.bind_distributor(self.pulp, repo.id, distributor.id) \
+                            for consumer in self.consumers for repo, _, distributor in consumer.repos]
 
-        # assert all bound repos are available on consumer
-        rpm_repo_ids = set([repo.id for repo in RpmRepo.list(self.consumer_cli)])
-        for repo, _, _ in self.repos:
-            self.assertIn(repo.id, rpm_repo_ids)
+        Task.wait_for_reports(self.pulp, task_reports)
+
+        # assert all bound repos are available on all consumer
+        for consumer in self.consumers:
+            rpm_repo_ids = {repo.id for repo in RpmRepo.list(consumer.cli)}
+            repo_ids = {repo.id for repo, _, _ in consumer.repos}
+            self.assertEqual(repo_ids, rpm_repo_ids & repo_ids, "consumer %s misses repos: %s" % \
+                (consumer, (repo_ids - (rpm_repo_ids & repo_ids))))
 
     def test_03_assert_yum_repos(self):
-        remote_yum_repos = YumRepo.list(self.consumer_cli)
-        for repo, _, _ in self.repos:
-            self.assertIn(YumRepo({'id': repo.id}), remote_yum_repos)
+        for consumer in self.consumers:
+            remote_yum_repos = YumRepo.list(consumer.cli)
+            for repo, _, _ in consumer.repos:
+                self.assertIn(YumRepo({'id': repo.id}), remote_yum_repos)
 
     def test_04_assert_unit_install(self):
         unit = {
             'name': 'zebra'
         }
-        Task.wait_for_report(
-            self.pulp,
-            self.consumer.install_unit(
-                self.pulp,
-                unit,
-                'rpm'
-            )
-        )
-        self.assertIn(
-            RpmUnit(unit, relevant_data_keys=unit.keys()),
-            RpmUnit.list(self.consumer_cli)
-        )
+        rpm = RpmUnit(unit, relevant_data_keys=unit.keys())
+        with self.pulp.asserting(True):
+            task_reports = [consumer.install_unit(self.pulp, unit, 'rpm') for consumer in self.consumers]
+        Task.wait_for_reports(self.pulp, task_reports)
+
+        for consumer in self.consumers:
+            assert rpm in RpmUnit.list(consumer.cli), "rpm %s not installed on %s" % (rpm, consumer)
 
     def test_05_assert_unit_uninstall(self):
         unit = {
             'name': 'zebra'
         }
-        self.assertIn(
-            RpmUnit(unit, relevant_data_keys=unit.keys()),
-            RpmUnit.list(self.consumer_cli)
-        )
-        Task.wait_for_report(
-            self.pulp,
-            self.consumer.uninstall_unit(
-                self.pulp,
-                unit,
-                'rpm'
-            )
-        )
-        self.assertNotIn(
-            RpmUnit(unit, relevant_data_keys=unit.keys()),
-            RpmUnit.list(self.consumer_cli)
-        )
+        rpm = RpmUnit(unit, relevant_data_keys=unit.keys())
+
+        for consumer in self.consumers:
+            assert rpm in RpmUnit.list(consumer.cli), "rpm %s not installed on %s" % (rpm, consumer)
+
+        with self.pulp.asserting(True):
+            task_reports = [consumer.uninstall_unit(self.pulp, unit, 'rpm') for consumer in self.consumers]
+        Task.wait_for_reports(self.pulp, task_reports)
+
+        for consumer in self.consumers:
+            assert rpm not in RpmUnit.list(consumer.cli), "rpm %s still installed on %s" % (rpm, consumer)
 
     def test_06_assert_nonexisten_unit_install(self):
         # https://bugzilla.redhat.com/show_bug.cgi?id=1062725
@@ -92,37 +113,33 @@ class CliConsumerTest(PulpTest):
         unit = {
             'name': '__NO_SUCH_UNIT__'
         }
-        Task.wait_for_report(
-            self.pulp,
-            self.consumer.install_unit(
-                self.pulp,
-                unit,
-                'rpm'
-            )
-        )
+        rpm = RpmUnit(unit, relevant_data_keys=unit.keys())
+        with self.pulp.asserting(True):
+            task_reports = [consumer.install_unit(self.pulp, unit, 'rpm') for consumer in self.consumers]
+        Task.wait_for_reports(self.pulp, task_reports)
 
     def test_07_assert_nonexisten_unit_uninstall(self):
         unit = {
             'name': '__NO_SUCH_UNIT__'
         }
-        Task.wait_for_report(
-            self.pulp,
-            self.consumer.uninstall_unit(
-                self.pulp,
-                unit,
-                'rpm'
-            )
-        )
+        rpm = RpmUnit(unit, relevant_data_keys=unit.keys())
+        with self.pulp.asserting(True):
+            task_reports = [consumer.uninstall_unit(self.pulp, unit, 'rpm') for consumer in self.consumers]
+        Task.wait_for_reports(self.pulp, task_reports)
 
     def test_08_unbind_repos(self):
         # assert unbinding distributors works
         with self.pulp.asserting(True):
-            for repo, _, distributor in self.repos:
-                Task.wait_for_report(self.pulp, self.consumer.unbind_distributor(self.pulp, repo.id, distributor.id))
+            task_reports = [consumer.unbind_distributor(self.pulp, repo.id, distributor.id) \
+                            for consumer in self.consumers for repo, _, distributor in consumer.repos]
+        Task.wait_for_reports(self.pulp, task_reports)
 
     @classmethod
     def tearDownClass(cls):
-        for repo, _, _ in cls.repos:
-            repo.delete(cls.pulp)
-        cls.consumer_cli.unregister()
+        with cls.pulp.asserting(True):
+            task_reports = [repo.delete(cls.pulp) for repo, _, _ in cls.repos]
+        Task.wait_for_reports(cls.pulp, task_reports)
+
+        for consumer in cls.consumers:
+            consumer.cli.unregister()
         super(CliConsumerTest, cls).tearDownClass()
