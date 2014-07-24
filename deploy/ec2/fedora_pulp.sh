f1de6e5d99493d27b817109e6febff04834a94e0
dparalen <vetrisko@gmail.com>
2014-07-24 13:48:58 +0200

    fix: utilize pulp tools to generate the certs and make them trusted system-wide

--- deploy/ec2/fedora_pulp.sh
+++ deploy/ec2/fedora_pulp.sh
@@ -80,13 +80,11 @@ yum -y groupinstall pulp-consumer
 sed -i s,host.*=.*,host=`hostname`, /etc/pulp/consumer/consumer.conf
 grep host= /etc/pulp/consumer/consumer.conf
 
-# generate ssl certs
-pushd /etc/pki/tls
-old_umask=`umask`
-umask 0077
-openssl req -new -x509 -nodes -out certs/localhost.crt -keyout private/localhost.key -subj "/C=US/ST=NC/L=Raleigh/CN=`hostname`"
-umask $old_umask
-chmod go+r certs/localhost.crt
+# generate ssl certs and ca
+pulp-gen-ca-certificate
+pushd /etc/pki/tls/certs
+# make the pulp ca.crt trusted system-wide
+ln -sf /etc/pki/pulp/ca.crt `openssl x509 -noout -hash -in /etc/pki/pulp/ca.crt`.0
 popd
 
 # enable services
