a3c527ae591548c4889f3f8c9901bd7d78222740
dparalen <vetrisko@gmail.com>
2015-01-29 18:15:12 +0100

    introducte a what-was-uploaded-was-downloaded assertion

--- tests/general_tests/test_21_upload_cud.py
+++ tests/general_tests/test_21_upload_cud.py
@@ -4,12 +4,16 @@ from pulp_auto.repo import create_yum_repo
 from pulp_auto.task import Task
 from contextlib import closing
 
-def upload_url_rpm(pulp, url):
-    '''create an upload object fed from the url'''
+def url_basename(url):
+    '''basename from url'''
     import os
     import urllib2
+    return os.path.basename(urllib2.urlparse.urlsplit(url).path)
+
+def upload_url_rpm(pulp, url):
+    '''create an upload object fed from the url'''
     # get basename for upload purpose
-    basename = os.path.basename(urllib2.urlparse.urlsplit(url).path)
+    basename = url_basename(url)
     with closing(temp_url(url)) as tmpfile:
         data = rpm_metadata(tmpfile.file)
         # augment rpm file name
@@ -40,9 +44,22 @@ class UploadCudTest(PulpTest):
         assert upload.id not in ids, 'upload %s deleted but still on server (%s)' % (upload.id, ids)
 
     def testcase_02_upload_rpm(self):
-        with deleting(self.pulp, create_yum_repo(self.pulp, 'upload_test_rpm_repo')[0]) as repo:
+        # create yum-repo, -importer, -distributor
+        with deleting(self.pulp, *create_yum_repo(self.pulp, 'upload_test_rpm_repo')) as (repo, (importer, (distributor))):
+            # create and perform an rpm url upload
             with deleting(self.pulp, upload_url_rpm(self.pulp, self.rpm_url)) as upload:
+                # assign the upload to the repo
                 response = upload.import_to(self.pulp, repo)
                 self.assertPulpOK()
                 Task.wait_for_report(self.pulp, response)
+                # check the content is accessible
+                response = repo.publish(self.pulp, distributor.id)
+                self.assertPulpOK()
+                Task.wait_for_report(self.pulp, response)
+                # fetch the package through the repo
+                pulp_rpm_url = distributor.content_url(self.pulp, url_basename(self.rpm_url))
+                with closing(temp_url(pulp_rpm_url)) as tmpfile:
+                    # make sure the rpm fetched has the same name as the one uploaded
+                    # rpm.name == 'bear' FIXME: a silly check indeed ;)
+                    assert url_basename(self.rpm_url).startswith(rpm_metadata(tmpfile)['unit_key']['name'])
 
