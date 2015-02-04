997cb2ee46315d546309d9990486ddcdfee62d4b
dparalen <vetrisko@gmail.com>
2015-02-04 04:29:39 +0100

    introducing parallel upload test case

--- tests/general_tests/test_21_upload_cud.py
+++ tests/general_tests/test_21_upload_cud.py
@@ -26,7 +26,9 @@ def upload_url_rpm(pulp, url):
     return upload
 
 class UploadCudTest(PulpTest):
-    rpm_url = 'https://repos.fedorapeople.org/repos/pulp/pulp/demo_repos/zoo/bear-4.1-1.noarch.rpm'
+    rpm_url_bear = 'https://repos.fedorapeople.org/repos/pulp/pulp/demo_repos/zoo/bear-4.1-1.noarch.rpm'
+    rpm_url_mouse = 'https://repos.fedorapeople.org/repos/pulp/pulp/demo_repos/zoo/mouse-0.1.12-1.noarch.rpm'
+
     def testcase_01_create_delete_upload(self):
         '''test creating uploads works'''
         upload = Upload.create(self.pulp)
@@ -43,23 +45,33 @@ class UploadCudTest(PulpTest):
         ids = Upload.list(self.pulp)
         assert upload.id not in ids, 'upload %s deleted but still on server (%s)' % (upload.id, ids)
 
+    @staticmethod
+    def rpm_uploader(pulp, url, repo, distributor):
+        '''perform an upload'''
+        # create an already fed upload object
+        with deleting(pulp, upload_url_rpm(pulp, url)) as upload:
+            # assing upload to repo
+            Task.wait_for_report(pulp, upload.import_to(pulp, repo))
+            # publish the content
+            Task.wait_for_report(pulp, repo.publish(pulp, distributor.id))
+            # download the rpm from pulp now
+            pulp_rpm_url = distributor.content_url(pulp, url_basename(url))
+            with closing(temp_url(pulp_rpm_url)) as tmpfile:
+                # make sure the rpm fetched has the same name as the one uploaded
+                # rpm.name == 'bear' FIXME: a silly check indeed ;)
+                assert url_basename(url).startswith(rpm_metadata(tmpfile)['unit_key']['name'])
+
+
     def testcase_02_upload_rpm(self):
         # create yum-repo, -importer, -distributor
         with deleting(self.pulp, *create_yum_repo(self.pulp, 'upload_test_rpm_repo')) as (repo, (importer, (distributor))):
             # create and perform an rpm url upload
-            with deleting(self.pulp, upload_url_rpm(self.pulp, self.rpm_url)) as upload:
-                # assign the upload to the repo
-                response = upload.import_to(self.pulp, repo)
-                self.assertPulpOK()
-                Task.wait_for_report(self.pulp, response)
-                # check the content is accessible
-                response = repo.publish(self.pulp, distributor.id)
-                self.assertPulpOK()
-                Task.wait_for_report(self.pulp, response)
-                # fetch the package through the repo
-                pulp_rpm_url = distributor.content_url(self.pulp, url_basename(self.rpm_url))
-                with closing(temp_url(pulp_rpm_url)) as tmpfile:
-                    # make sure the rpm fetched has the same name as the one uploaded
-                    # rpm.name == 'bear' FIXME: a silly check indeed ;)
-                    assert url_basename(self.rpm_url).startswith(rpm_metadata(tmpfile)['unit_key']['name'])
+            self.rpm_uploader(self.pulp, self.rpm_url_bear, repo, distributor)
 
+    def testcase_03_parallel_upload_rpms(self):
+        import gevent
+        with deleting(self.pulp, *create_yum_repo(self.pulp, 'upload_test_rpm_repo')) as (repo, (importer, (distributor))):
+            # create and perform an rpm url upload
+            jobs = [gevent.spawn(lambda: self.rpm_uploader(self.pulp, url, repo, distributor)) for url in \
+                            [self.rpm_url_bear, self.rpm_url_mouse]]
+            gevent.joinall(jobs, raise_error=True)
