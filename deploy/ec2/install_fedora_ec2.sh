6e73ae8275deb567b743dc378cb7a1fefb434884
root <root@dhcp130-229.brq.redhat.com>
2015-02-11 14:27:11 +0100

    scripts separated

--- deploy/ec2/install_fedora_ec2.sh
+++ deploy/ec2/install_fedora_ec2.sh
@@ -6,12 +6,12 @@ set -xe
 # while ! [ -x /sbin/ldconfig ] ; do sleep 1 ; echo -n . ; done
 # echo
 
-# # enable root access - not allowed by default
+## enable root access
 cat ~fedora/.ssh/authorized_keys > ~/.ssh/authorized_keys
 sed -i s,disable_root:.*,disable_root:0, /etc/cloud/cloud.cfg
 grep disable_root /etc/cloud/cloud.cfg
 
-# # hostname - internal and external hostname at amazon cloud differs
+## hostname
 hostname `curl -# http://169.254.169.254/latest/meta-data/public-hostname`
 grep HOSTNAME= /etc/sysconfig/network || echo HOSTNAME=`hostname` >> /etc/sysconfig/network
 sed -i s,HOSTNAME=.*$,HOSTNAME=`hostname`, /etc/sysconfig/network
@@ -19,44 +19,15 @@ grep HOSTNAME= /etc/sysconfig/network
 echo `curl -# http://169.254.169.254/latest/meta-data/public-ipv4` `hostname` >> /etc/hosts
 tail -1 /etc/hosts
 
-#TODO how to run it?
-#
+
 # PULP install
 #
-# curl https://raw.githubusercontent.com/kvitajakub/pulp-automation/master/deploy/ec2/install_pulp.sh | sed "s/exec &>/exec &>>/"
+curl https://raw.githubusercontent.com/kvitajakub/pulp-automation/master/deploy/ec2/install_pulp.sh | su - root -s /bin/sh &>> /var/log/fedora_pulp_amazon.log
 
 # configure firewall
-iptables -I INPUT -p tcp --destination-port 443 -j ACCEPT
-iptables -I INPUT -p tcp --destination-port 5672 -j ACCEPT
-service iptables save ||
-
-
-# enable services
-systemctl enable mongod.service
-systemctl start mongod.service || systemctl start mongod.service # sometimes it just takes too long and gets killed the first time
-systemctl enable qpidd.service
-systemctl start qpidd.service
-
-# init db
-pulp-manage-db
-
-# start apache
-systemctl enable httpd.service
-systemctl start httpd.service
-
-# start consumer service
-systemctl enable goferd.service
-systemctl start goferd.service
-
-#celery section
-#enable pulp workers to perform ditributed tasks
-systemctl enable pulp_workers
-systemctl start pulp_workers
-systemctl enable pulp_celerybeat
-systemctl start pulp_celerybeat
-# this process acts as a task router, deciding which worker should perform certain types of tasks.
-systemctl enable pulp_resource_manager
-systemctl start pulp_resource_manager
+# iptables -I INPUT -p tcp --destination-port 443 -j ACCEPT
+# iptables -I INPUT -p tcp --destination-port 5672 -j ACCEPT
+# service iptables save ||
 
 
 # TODO copy from the other
