4f11cd418ba80d216a7f37b85f75ab9ae7cbf5d9
Jakub Kvita <kvitajakub@gmail.com>
2015-02-20 15:32:25 +0100

    script for splitting installation on rhel6, fixed some repo paths

---
+++ deploy/ec2/install_rhel6_pulp.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+
+
+exec &> /var/log/fedora_pulp.log
+set -xe
+
+while ! [ -x /sbin/ldconfig ] ; do sleep 1 ; echo -n . ; done
+echo
+
+function service_start {
+    chkconfig $1 on
+    service $1 start
+}
+
+# enable root access
+#
+#
+###################
+#CHECK fix username on cloud type
+#  OS1 Cloud  -  cloud-user
+#  Amazon EC2 -  fedora?
+##################
+#
+#
+#
+cat ~cloud-user/.ssh/authorized_keys > ~/.ssh/authorized_keys
+sed -i s,disable_root:.*,disable_root:0, /etc/cloud/cloud.cfg
+grep disable_root /etc/cloud/cloud.cfg
+
+# hostname
+hostname `curl -# http://169.254.169.254/latest/meta-data/public-hostname`
+grep HOSTNAME= /etc/sysconfig/network || echo HOSTNAME=`hostname` >> /etc/sysconfig/network
+sed -i s,HOSTNAME=.*$,HOSTNAME=`hostname`, /etc/sysconfig/network
+grep HOSTNAME= /etc/sysconfig/network
+echo `curl -# http://169.254.169.254/latest/meta-data/public-ipv4` `hostname` >> /etc/hosts
+tail -1 /etc/hosts
+
+# fetch pulp repo file
+pushd /etc/yum.repos.d/
+cat <<PULP_REPO_EOF > rhel6-pulp.repo
+# Weekly Testing Builds
+[pulp-v2-testing]
+name=Pulp v2 Testing Builds
+baseurl=http://repos.fedorapeople.org/repos/pulp/pulp/testing/2.6/\$releasever/\$basearch/
+enabled=1
+skip_if_unavailable=1
+gpgcheck=0
+PULP_REPO_EOF
+
+cat <<EPEL_REPO_EOF > rhel6-epel.repo
+[epel]
+name=Extra Packages for Enterprise Linux 6 - \$basearch
+#baseurl=http://download.fedoraproject.org/pub/epel/6/\$basearch
+mirrorlist=https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=\$basearch
+failovermethod=priority
+enabled=1
+gpgcheck=0
+EPEL_REPO_EOF
+
+cat <<QPID_REPO_EOF > qpid-cpp.repo
+[qpid-cpp-x86_64]
+name=Qpid packages for epel-6 \$basearch
+baseurl=http://repos.fedorapeople.org/repos/mcpierce/qpid-cpp/epel-6/\$basearch
+enabled=1
+skip_if_unavailable=1
+gpgcheck=0
+
+[qpid-cpp-noarch]
+name=Qpid packages for epel-6 noarch
+baseurl=http://repos.fedorapeople.org/repos/mcpierce/qpid-cpp/epel-6/noarch
+enabled=1
+skip_if_unavailable=1
+gpgcheck=0
+
+[qpid-cpp-source]
+name=Qpid packages for epel-6 - Source
+baseurl=http://repos.fedorapeople.org/repos/mcpierce/qpid-cpp/epel-6/SRPMS
+enabled=0
+skip_if_unavailable=1
+QPID_REPO_EOF
+popd
+
+# install pulp
+yum update -y selinux-policy-targeted ||: # avoid  https://bugzilla.redhat.com/show_bug.cgi?id=877831
+yum update -y
+# FIXME --- postinstall scriptlets failing...
+yum -y install mongodb-server
+yum -y install qpid-cpp-server
+yum -y groupinstall pulp-server
+yum -y install python-qpid-qmf
+
+# configure firewall
+iptables -I INPUT -p tcp --destination-port 443 -j ACCEPT
+iptables -I INPUT -p tcp --destination-port 5672 -j ACCEPT
+service iptables save
+
+# configure pulp
+sed -i s,url:.*tcp://.*:5672,url:tcp://`hostname`:5672, /etc/pulp/server.conf
+grep url:.*:5672 /etc/pulp/server.conf
+
+#configure borker_url
+sed -i s,^[#\ ]*broker_url:.*qpid://guest@localhost/,broker_url:qpid://`hostname`:5672/, /etc/pulp/server.conf
+grep broker_url:.*:5672/ /etc/pulp/server.conf
+
+# configure qpidd
+grep auth= /etc/qpidd.conf || echo auth=no >> /etc/qpidd.conf
+sed -i s,auth=.*$,auth=no, /etc/qpidd.conf
+grep auth= /etc/qpidd.conf
+
+# configure pulp-admin
+yum -y groupinstall pulp-admin
+sed -i s,host.*=.*,host=`hostname`, /etc/pulp/admin/admin.conf
+grep host= /etc/pulp/admin/admin.conf
+
+# configure local consumer
+yum -y groupinstall pulp-consumer
+sed -i s,host.*=.*,host=`hostname`, /etc/pulp/consumer/consumer.conf
+grep host= /etc/pulp/consumer/consumer.conf
+
+#install Docker plugins
+yum install -y pulp-docker-admin-extensions pulp-docker-plugins python-pulp-docker-common
+
+# generate ssl certs and ca
+pulp-gen-ca-certificate
+pushd /etc/pki/tls/certs
+# make the pulp ca.crt trusted system-wide
+ln -sf /etc/pki/pulp/ca.crt `openssl x509 -noout -hash -in /etc/pki/pulp/ca.crt`.0
+popd
+
+pushd /etc/qpid/
+cat << QPIDD_EOF > qpidd.conf
+ssl-require-client-authentication=no
+auth=no
+log-to-syslog=yes
+log-enable=info+
+log-time=yes
+log-source=yes
+log-function=yes
+QPIDD_EOF
+popd
+
+#Workaround bug 1121102
+rpm -q --queryformat '%{VERSION}' pulp-server | grep '2\.4\.0' && sed -i s,processes=.,processes=1, /etc/httpd/conf.d/pulp.conf
+
+# enable services
+  service_start mongod
+  service_start qpidd
+  su - apache -s /bin/sh -c pulp-manage-db
+  service_start httpd
+  service_start goferd
+  service_start pulp_workers
+  service_start pulp_celerybeat
+  service_start pulp_resource_manager
+
