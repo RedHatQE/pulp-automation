72dbe47b6445a9ea138f2aa259fa9e530a996983
Jakub Kvita <kvitajakub@gmail.com>
2015-03-26 15:55:14 +0100

    test case deleted, was not working properly, waiting to be created anew

--- tests/regression_tests/test_02_iso_repo.py
+++ tests/regression_tests/test_02_iso_repo.py
@@ -8,23 +8,17 @@ from pulp_auto.units import Orphans
 def setUpModule():
     pass
 
+def tearDownModule():
+    pass
 
 class IsoRepoTest(pulp_test.PulpTest):
     @classmethod
     def setUpClass(cls):
         super(IsoRepoTest, cls).setUpClass()
-        cls.repo = Repo(data={'id': cls.__name__ + "_repo",
-                              "_repo-type": "iso-repo"})
-        cls.repo2 = Repo(data={'id': cls.__name__ + "_repo2",
-                               "_repo-type": "iso-repo"})
-        cls.feed = 'https://repos.fedorapeople.org/repos/pulp/pulp/demo_repos/test_file_repo/'
+        # cls.repo = Repo(data={'id': cls.__name__ + "_repo",
+        #                       "_repo-type": "iso-repo"})
+        # cls.repo2 = Repo(data={'id': cls.__name__ + "_repo2",
+        #                        "_repo-type": "iso-repo"})
+        # cls.feed = 'https://repos.fedorapeople.org/repos/pulp/pulp/demo_repos/test_file_repo/'
+        pass
 
-
-class SimpleIsoRepoTest(IsoRepoTest):
-
-    def test_01_create_repo(self):
-        self.repo.create(self.pulp)
-        self.assertPulpOK()
-        #no duplicate repo
-        self.repo.create(self.pulp)
-        self.assertPulp(code=409)
