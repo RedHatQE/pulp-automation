572bb51c01c36355ad3535fc187d9600bd9c2c05
Peter Lacko <placko@redhat.com>
2015-08-28 11:56:09 +0200

    setup fixed

--- setup.py
+++ setup.py
@@ -26,9 +26,9 @@ setup(name='pulp_auto',
       url='https://github.com/RedHatQE/pulp-automation',
       license='GPLv3+',
       install_requires=['nose>=1.3.0', 'requests>=1.2.3', 'PyYAML',
-            'gevent>=1.0.1', 'stitches', 'M2Crypto', 'requestbin>0.1'],
+            'gevent>=1.0.1', 'stitches', 'M2Crypto', 'requestbin>0.1', 'qpid-python'],
       dependency_links=[
-            "svn+http://svn.apache.org/repos/asf/qpid/trunk/qpid/python/"
+            "svn+http://svn.apache.org/repos/asf/qpid/branches/0.32/qpid/python/"
       ],
       classifiers=[
           'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
